6dd5f2a406c0c0e9f4fb6566d165c037e1934343
ORG-www <sharmachaitanyaind@gmail.com>
2024-01-18 17:52:34 +0530

    completed setstate background change

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,50 @@
 
-
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CheckBox(),
-    );
-  }
+  State<MyApp> createState() => MyAppState();
 }
 
-class CheckBox extends StatefulWidget {
+class MyAppState extends State<MyApp> {
+  Color backgroundColor = Colors.blueGrey;
 
-  const CheckBox({Key? key}) : super(key: key);
-
-  @override
-  State<CheckBox> createState() => _MyFormState();
-}
-
-class _MyFormState extends State<CheckBox> {
-
-  final StreamController<bool> _checkBoxController = StreamController();
-  Stream<bool> get _checkBoxStream => _checkBoxController.stream;
-
-  @override
-  void dispose() {
-    _checkBoxController.close();
-    super.dispose();
+  void changeColor(Color color) {
+    setState(() {
+      backgroundColor = color;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Checkbox with stream builder"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: backgroundColor,
 
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Name",
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => changeColor(Colors.white),
+                child: const Text('Customer'),
               ),
-            ),
-            const SizedBox(height: 10,),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Phone",
+              ElevatedButton(
+                onPressed: () => changeColor(Colors.redAccent),
+                child: const Text('Agent'),
               ),
-            ),
-            const SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text("Accept"),
-                StreamBuilder(
-                    stream: _checkBoxStream,
-                    initialData: false,
-                    builder: (BuildContext context, AsyncSnapshot<bool> snapshot ){
-                      return Checkbox(
-                          value: snapshot.data,
-                          onChanged: (changedValue){
-                            _checkBoxController.sink.add(changedValue!);
-                          }
-                      );
-                    }),
-              ],
-            ),
-            const SizedBox(height: 10,),
-            ElevatedButton(onPressed: (){},
-                child: const Text("Submit"))
-          ],
+              ElevatedButton(
+                onPressed: () => changeColor(Colors.green),
+                child: const Text('Merchant'),
+              ),
+            ],
+          ),
         ),
       ),
     );
