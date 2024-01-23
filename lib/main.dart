7eea006fdbc9fd52d2bedcb45fca3736c10b2a02
ORG-www <sharmachaitanyaind@gmail.com>
2024-01-23 11:19:37 +0530

    completed future builder with list view builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+
+import 'package:assignment_4/src/views/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,55 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FutureBuilderScreen(),
+      title: 'API Fetch Example',
+      home: HomePage(),
     );
   }
 }
 
-class FutureBuilderScreen extends StatefulWidget {
-  const FutureBuilderScreen({super.key});
 
-  @override
-  State<FutureBuilderScreen> createState() => _FutureBuilderScreenState();
-}
 
-class _FutureBuilderScreenState extends State<FutureBuilderScreen> {
-
-  String img = '';
-
-  Future<String> fetchData() async {
-
-    await Future.delayed(const Duration(seconds: 2));
-    img = 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg';
-    return img;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FutureBuilder'),
-      ),
-      body: Center(
-        child: FutureBuilder(
-          future: fetchData(),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              return Center(
-                child: Image(
-                  image: NetworkImage('${snapshot.data}'),
-                ),
-              );
-            } else {
-              return const Text('No data available.');
-            }
-          },
-        ),
-      ),
-    );
-  }
-}
